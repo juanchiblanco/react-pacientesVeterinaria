3f4ee68747dc509807bd8d332259ce9b94f9ecdc
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-23 00:53:33 -0300

    disenio del form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioPaciente from "./components/FormularioPaciente";
 
 function App() {
 
   return (
     <main className="container my-3">
     <h1 className="text-center">Administrador de pacientes</h1>
+    <FormularioPaciente/>
     </main>
   )
 }
--- src/components/FormularioPaciente.jsx
+++ src/components/FormularioPaciente.jsx
@@ -1,9 +1,46 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+
 const FormularioPaciente = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Card className="shadow my-3">
+      <Card.Header className="ps-5 py-3 bg-light lead">Llenar el formulario para crear un cita</Card.Header>
+      <Card.Body className="bg-primary-subtle">
+        <Form>
+          <Form.Group className="mb-3" controlId="nombreMascota">
+            <Form.Label>Nombre de la mascota *</Form.Label>
+            <Form.Control type="name" placeholder="Ej: Chispa" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="nombreDuenio">
+            <Form.Label>Nombre del dueño/a *</Form.Label>
+            <Form.Control type="name" placeholder="Ej: Juan Perez" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="fecha">
+            <Form.Label>Fecha *</Form.Label>
+            <Form.Control type="date" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="hora">
+            <Form.Label>Hora *</Form.Label>
+            <Form.Control
+              type="time"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="sintoma">
+            <Form.Label>Sintomas *</Form.Label>
+            <Form.Control
+              type="text"
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="mt-4 w-100 shadow">
+            Agregar nueva cita
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default FormularioPaciente;
